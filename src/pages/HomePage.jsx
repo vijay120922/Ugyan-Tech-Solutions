faa43e14f7441d2ea16ac323065a550ca2fc0387
vijay120922 <n200305@rguktn.ac.in>
2025-06-18 08:26:56 +0530

    nav bar completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import Navbar from '../components/Navbar';
+import './HomePage.css'; 
 const HomePage = () => {
     return (
         <>
-     
+        <div
+      className="h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/homebg.jpeg')" }}
+    >
         <Navbar />
+        
+      
+    </div>
+     
+        
 
         </>
         
