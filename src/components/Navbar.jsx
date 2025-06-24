299414b254680c67053574028662b5b7c922b7d5
29Anirudh <anirudhbatana@gmail.com>
2025-06-24 17:50:08 +0530

    Navbar changes done!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,10 @@ const Navbar = ({ user }) => {
     <nav
       className={`${
         isAtTop && location.pathname === "/" ? "bg-transparent" : "gradient-bg shadow-xl shadow-gray-500"
-      } w-full fixed top-0 z-50 transition-all duration-150 rounded-b-3xl`}
+      } w-full fixed top-0 z-50 transition-all duration-150 rounded-b-3xl p-5 lg:p-0`}
     >
-      <div className="flex justify-between items-center p-4 max-w-[1200px] mx-auto">
+      
+      <div className="flex justify-between items-center w-full mx-auto">
         <img
           className="object-contain w-[130px] cursor-pointer"
           src="/logo1.png"
@@ -34,36 +35,34 @@ const Navbar = ({ user }) => {
           onClick={() => nav("/")}
         />
 
-        {/* Hamburger icon for small screens */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button onClick={() => setMenuOpen(!menuOpen)}>
             {menuOpen ? <X size={26} /> : <Menu size={26} />}
           </button>
         </div>
 
-        {/* Main nav menu */}
         <div
-          className={`nav-links md:flex gap-10 items-center text-[1em] ${
-            menuOpen ? "flex flex-col absolute top-[70px] left-0 w-full bg-white p-6 shadow-md rounded-b-xl z-40" : "hidden"
-          } md:static md:flex-row md:bg-transparent`}
+          className={`nav-links lg:flex gap-2 w-full items-center md:justify-start lg:justify-between text-[1em] ${
+            menuOpen ? "flex flex-col absolute top-[70px] left-0 w-full bg-white p-6 rounded-b-xl z-40" : "hidden"
+          } lg:static lg:flex-row lg:bg-transparent`}
         >
-          <NavLink to="/" className="nav-item" style={{ "--i": 1 }}>
+          <NavLink to="/" className="nav-item" style={{ "--i": 1 }} onClick={()=>setMenuOpen(false)} >
             Home
           </NavLink>
-          <NavLink to="/courses" className="nav-item" style={{ "--i": 2 }}>
+          <NavLink to="/courses" className="nav-item" style={{ "--i": 2 }} onClick={()=>setMenuOpen(false)}>
             Courses
           </NavLink>
-          <NavLink to="/why-choose-us" className="nav-item" style={{ "--i": 3 }}>
+          <NavLink to="/why-choose-us" className="nav-item" style={{ "--i": 3 }} onClick={()=>setMenuOpen(false)}>
             Why Choose Us
           </NavLink>
-          <NavLink to="/about-us" className="nav-item" style={{ "--i": 4 }}>
+          <NavLink to="/about-us" className="nav-item" style={{ "--i": 4 }} onClick={()=>setMenuOpen(false)}>
             About Us
           </NavLink>
-          <NavLink to="/contact-us" className="nav-item" style={{ "--i": 5 }}>
+          <NavLink to="/contact-us" className="nav-item" style={{ "--i": 5 }} onClick={()=>setMenuOpen(false)}>
             Contact Us
           </NavLink>
 
-          <button className="login-btn mt-4 md:mt-0" onClick={() => nav('/loginorSignup')}>
+          <button className="login-btn mt-4 md:mt-0" onClick={() => {nav('/loginorSignup');setMenuOpen(false)}} >
             Login / Sign Up
             <span className="login-arrow">
               <ArrowRight size={16} />
