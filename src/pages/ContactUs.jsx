359d4c357402ec00f2e83b1771f1a3dab3fac496
vijay120922 <n200305@rguktn.ac.in>
2025-06-25 21:14:10 +0530

    original details updated

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -11,9 +11,9 @@ const ContactUs = () => {
         {/* Contact Info */}
         <div className="contact-us-info">
           <h2>Get in Touch</h2>
-          <p><strong>Address:</strong> 123 Learning Lane, Edutech City, IN</p>
-          <p><strong>Phone:</strong> +91 98765 43210</p>
-          <p><strong>Email:</strong> contact@edutech.com</p>
+          <p><strong>Address:</strong> Banglore, India</p>
+          <p><strong>Phone:</strong> +91 79751 65470</p>
+          <p><strong>Email:</strong> support@ugyan.in</p>
           <p><strong>Hours:</strong> Mon - Fri, 9AM - 6PM</p>
 
           <div className="contact-us-social-links">
@@ -38,12 +38,13 @@ const ContactUs = () => {
         </div>
       </div>
       <div className="contact-us-map">
-        <iframe
+        {/* <iframe
           title="Google Map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.219340903797!2d144.96332!3d-37.814217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d43f0a66b0d%3A0x5045675218ce6e0!2sIndia!5e0!3m2!1sen!2sin!4v1610000000000"
           allowFullScreen=""
           loading="lazy"
-        ></iframe>
+        ></iframe> */}
+        <iframe title="Google Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62218.11065641293!2d77.66988807060254!3d12.931362919166437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x659d12bf5498f4e7%3A0x8ef08c377c391150!2sUGYAN%20PRIVATE%20LIMITED!5e0!3m2!1sen!2sin!4v1750866136697!5m2!1sen!2sin" allowfullscreen="" loading="lazy" ></iframe>
       </div>
     </div>
   );
