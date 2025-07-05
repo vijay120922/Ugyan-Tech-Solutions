f21649e6f489f158f9326d8e851953e6c9965025
charanijatavathu <charanijatavathu1001@gmail.com>
2025-07-05 12:06:57 +0530

    changes done to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         
-        {/* Company Info */}
-        <div className="footer-section company-info">
-          <h2 className="footer-logo">Ugyan Edutech</h2>
-          <p>Empowering Learning, Empowering Lives</p>
-        </div>
-
         {/* Quick Links */}
         <div className="footer-section">
           <h3>Quick Links</h3>
@@ -25,32 +21,44 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Contact Info */}
-        <div className="footer-section">
-          <h3>Contact Us</h3>
-          <p>Email: support@ugyan.in</p>
-          <p>Phone: +91 79751 65470</p>
-          <p>Location:</p>
-          <div className="map-wrapper">
-            <iframe 
-              title="Google Map" 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62218.11065641293!2d77.66988807060254!3d12.931362919166437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x659d12bf5498f4e7%3A0x8ef08c377c391150!2sUGYAN%20PRIVATE%20LIMITED!5e0!3m2!1sen!2sin!4v1750866136697!5m2!1sen!2sin" 
-              allowFullScreen 
-              loading="lazy">
-            </iframe>
-          </div>
+        {/* Company Info + Social Media */}
+        <div className="footer-section company-info">
+          <h2 className="footer-logo">Ugyan Edutech</h2>
+          <p>Empowering Learning, Empowering Lives</p>
+          
+          <h3 className="follow">Follow Us</h3>
+        <div className="social-icons">
+  <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+  <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+  <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+  <a href="#"><FontAwesomeIcon icon={faGlobe} /></a>
+</div>
+
         </div>
 
-        {/* Social Media */}
-        <div className="footer-section">
-          <h3>Follow Us</h3>
-          <div className="social-icons">
-            <a href="#" aria-label="LinkedIn"><i className="fab fa-linkedin-in"></i></a>
-            <a href="#" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
-            <a href="#" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
-            <a href="#" aria-label="YouTube"><i className="fab fa-youtube"></i></a>
-          </div>
-        </div>
+        {/* Contact Info */}
+<div className="footer-section">
+  <h3>Contact Us</h3>
+  <p>Email: support@ugyan.in</p>
+  <p>Phone: +91 79751 65470</p>
+  <p>
+    Location: Seetharampalya, 
+    <br>
+    </br>
+    Bangalore -India
+    <a
+      href="https://www.google.com/maps/search/?api=1&query=Seetharampalya+Bangalore"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="location-link"
+    >
+      <br/>
+      Click to View on Map
+    </a>
+  </p>
+</div>
+
+
       </div>
 
       <div className="footer-bottom">
